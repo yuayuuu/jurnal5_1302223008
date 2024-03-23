84bd0a22f65183b034dcefd49967fe45abaaa696
yuayuuu <4yulestarii@gmail.com>
2024-03-23 07:58:11 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jurnal5_1302223008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
